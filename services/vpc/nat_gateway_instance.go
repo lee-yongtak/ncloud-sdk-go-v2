33c01a00afeb9567b1440c6dc3b2b38530a77f48
sangkyu.kim <sangkyu.kim@navercorp.com>
2020-07-28 14:40:50 +0900

    add VpcNo, ZoneCode

--- services/vpc/nat_gateway_instance.go
+++ services/vpc/nat_gateway_instance.go
@@ -10,31 +10,36 @@
 package vpc
 
 type NatGatewayInstance struct {
+	// VPC번호
+	VpcNo *string `json:"vpcNo"`
 
 	// VPC이름
-VpcName *string `json:"vpcName,omitempty"`
+	VpcName *string `json:"vpcName,omitempty"`
 
 	// NATGateway인스턴스번호
-NatGatewayInstanceNo *string `json:"natGatewayInstanceNo,omitempty"`
+	NatGatewayInstanceNo *string `json:"natGatewayInstanceNo,omitempty"`
 
 	// NATGateway이름
-NatGatewayName *string `json:"natGatewayName,omitempty"`
+	NatGatewayName *string `json:"natGatewayName,omitempty"`
 
 	// 공인IP주소
-PublicIp *string `json:"publicIp,omitempty"`
+	PublicIp *string `json:"publicIp,omitempty"`
 
 	// NATGateway인스턴스상태
-NatGatewayInstanceStatus *CommonCode `json:"natGatewayInstanceStatus,omitempty"`
+	NatGatewayInstanceStatus *CommonCode `json:"natGatewayInstanceStatus,omitempty"`
 
 	// NATGateway인스턴스상태이름
-NatGatewayInstanceStatusName *string `json:"natGatewayInstanceStatusName,omitempty"`
+	NatGatewayInstanceStatusName *string `json:"natGatewayInstanceStatusName,omitempty"`
 
 	// NATGateway인스턴스OP
-NatGatewayInstanceOperation *CommonCode `json:"natGatewayInstanceOperation,omitempty"`
+	NatGatewayInstanceOperation *CommonCode `json:"natGatewayInstanceOperation,omitempty"`
 
 	// 생성일시
-CreateDate *string `json:"createDate,omitempty"`
+	CreateDate *string `json:"createDate,omitempty"`
 
 	// NATGateway설명
-NatGatewayDescription *string `json:"natGatewayDescription,omitempty"`
+	NatGatewayDescription *string `json:"natGatewayDescription,omitempty"`
+
+	// 존(Zone) 코드
+	ZoneCode *string `json:"zoneCode,omitempty"`
 }
